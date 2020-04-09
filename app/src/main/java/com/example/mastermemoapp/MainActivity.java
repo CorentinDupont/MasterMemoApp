dffddfe37f468e48f832817f3b6444a7dd03fe8c
Corentin Dupont <corentin.dupont@helyx.dev>
2020-04-09 15:42:46 +0200

    📦 init project

---
+++ app/src/main/java/com/example/mastermemoapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mastermemoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+    }
+}
